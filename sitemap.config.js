257ae87c390f0739d408190b092b4ce42c31b6fe
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-04-29 19:52:32 +0900

    Create sitemap.config.js

---
+++ sitemap.config.js
@@ -0,0 +1,9 @@
+// sitemap.config.js
+// https://fwywd.com
+
+module.exports = {
+  siteUrl: 'https://minecraft-wikinote.vercel.app',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: './out',
+};
