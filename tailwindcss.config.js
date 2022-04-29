f8f97c18abc4dc9ad0c3cf471bc2aef0e78dfb06
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-04-29 19:54:43 +0900

    Update tailwindcss.config.js

--- tailwindcss.config.js
+++ tailwindcss.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: ["./pages/**/*.{jsx,js}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [],
 }
