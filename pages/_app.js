55fd96db2c038065f1b835d8001c46748fd1b774
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-04-30 10:04:02 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 // import { router } from "next/router";
 // import twemoji from "twemoji";
+import NavBar from '/components/NavBar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NavBar />
       <Component {...pageProps} />
     </>
   );
