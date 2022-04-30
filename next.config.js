7a837f978a58c27e8c3725ff0df910af8d012f99
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-04-30 09:42:43 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
-    rehypePlugins: []
+    rehypePlugins: [],
     // If you use `MDXProvider`, uncomment the following line.
     providerImportSource: "@mdx-js/react",
   }
