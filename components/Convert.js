8c99550d7b94a3451e7663b90ab3e04af63a78ad
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-01 16:38:07 +0900

    ** Customize some components

--- components/Convert.js
+++ components/Convert.js
@@ -1,7 +1,7 @@
 export function HeadTitle({ children }) {
   return (
     <>
-      <h1 className="font-bold text-xl text-base-900">{children}</h1>
+      <h1 className="font-bold text-2xl text-base-900">{children}</h1>
       <div className="divider" />
     </>
   )
@@ -10,7 +10,7 @@ export function HeadTitle({ children }) {
 export function ContentH2({ children }) {
   return (
     <>
-      <div className="border-dashed border-y-4 bg-base-500">
+      <div className="border-dashed border-y-4 bg-base-500 pt-6 pb-3">
         <h2 className="font-semibold text-lg text-base-800">{children}</h2>
       </div>
     </>
@@ -20,8 +20,8 @@ export function ContentH2({ children }) {
 export function ContentCode({ children }) {
   return (
     <>
-      <div className="rounded-lg bg-base-content">
-        <code className="p-2">{children}</code>
+      <div className="rounded-lg bg-base-content pt-2 pb-4 shadow-md shadow-base-content/50">
+        <code className="px-2 z-10 text-base-200 py-3">{children}</code>
       </div>
     </>
   )
