28cb8fd2cc9560409ba84559e649fef8c89df364
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-01 10:41:20 +0900

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -58,6 +58,20 @@ export default function NavBar() {
               />
             </svg>
           </button>
+          <div className="dropdown dropdown-end">
+            <label className="btn btn-square btn-ghost">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+            </label>
+            <ul tabindex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+              <li>
+                <select data-choose-theme className="select select-bordered">
+                  <option value="">Default</option>
+                  <option value="dark">Dark</option>
+                  <option value="corporate">Corporate</option>
+                </select>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
