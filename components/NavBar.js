9c496d31313337dfe42ef7168934fe8ebb0941e8
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-01 10:44:27 +0900

    [deploy] themes

--- components/NavBar.js
+++ components/NavBar.js
@@ -62,7 +62,7 @@ export default function NavBar() {
             <label className="btn btn-square btn-ghost">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
             </label>
-            <ul tabindex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+            <ul tabIndex="0" className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
               <li>
                 <select data-choose-theme className="select select-bordered">
                   <option value="">Default</option>
