f99b941cb8fe5b1a3e867d0214339f849d91358d
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-01 20:11:32 +0900

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <div>
+          <p>Copyright © 2022 - WikiNotes</p>
+          <p>"Minecraft" is a trademark of Mojang Studios. This website is not affiliated with Mojang Studios.</p>
+        </div>
+      </footer>
+    </>
+   )
+}
