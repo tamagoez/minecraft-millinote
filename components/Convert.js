c5a8ca84d4d60111d5fd5ddd26f46b73e5a60c8c
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-01 17:49:55 +0900

    * Update components

--- components/Convert.js
+++ components/Convert.js
@@ -3,8 +3,7 @@ import Link from 'next/link'
 export function HeadTitle({ children }) {
   return (
     <>
-      <h1 className="font-bold text-2xl text-base-900">{children}</h1>
-      <div className="divider" />
+      <h1 className="font-bold text-2xl text-base-900 border-solid pb-2">{children}</h1>
     </>
   )
 }
@@ -12,7 +11,7 @@ export function HeadTitle({ children }) {
 export function ContentH2({ children }) {
   return (
     <>
-      <div className="border-dashed border-b-4 bg-base-500 pt-6 pb-3">
+      <div className="border-dashed border-b-4 bg-base-500 pt-6 pb-2">
         <h2 className="font-semibold text-lg text-base-800">{children}</h2>
       </div>
     </>
@@ -23,7 +22,7 @@ export function ContentCode({ children }) {
   return (
     <>
       <div className="rounded-lg bg-base-content pt-2 pb-4 shadow-md shadow-base-content/50">
-        <code className="px-2 z-10 text-base-200 py-3">{children}</code>
+        <code className="px-2 z-10 text-base-200 py-3 overflow-x-auto">{children}</code>
       </div>
     </>
   )
