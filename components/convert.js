410139fe6d5e314b5d7b0b098c142c53e9bbae03
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-01 13:37:34 +0900

    Create convert.js

---
+++ components/convert.js
@@ -0,0 +1,8 @@
+export function HeadTitle({ children }){
+  return (
+    <>
+      <h1 className="font-medium text-xl text-base-900">{children}</h1>
+      <div className="divider" />
+    </>
+  )
+}
