97c4c70d50199ed49eeec6c618be1adc0e754fe9
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-01 10:16:46 +0900

    Create vercel-ignored-build-step.sh

---
+++ files/vercel-ignored-build-step.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# https://zenn.dev/catnose99/articles/b37104fc7ef214
+# https://vercel.com/docs/concepts/projects/environment-variables#system-environment-variables
+
+COMMIT_MESSAGE=$VERCEL_GIT_COMMIT_MESSAGE
+echo "Got Comment: $COMMIT_MESSAGE"
+
+if [[ "$COMMIT_MESSAGE" =~ "deploy" ]] ; then
+  # Proceed with the build
+  echo "✅ - Build can proceed"
+  exit 1;
+else
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+fi
