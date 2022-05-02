61d15c1aac70ef8ef0a4d6c3ad1ee2bcafc6db89
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-02 18:31:06 +0900

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,9 @@
 export default function Footer() {
   return (
     <>
-      <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <footer className="footer footer-center p-4 bg-base-200 text-base-content">
         <div>
-          <p>Copyright © 2022 - WikiNotes</p>
+          <p>Copyright © 2022 - WikiNote</p>
           <p>&quot;Minecraft&quot; is a trademark of Mojang Studios. This website is not affiliated with Mojang Studios.</p>
         </div>
       </footer>
