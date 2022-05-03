01e3c07a4156837d71255b2e506c9f1aeb0b3f0b
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-03 21:04:09 +0900

    * Some update!

--- components/NavBar.js
+++ components/NavBar.js
@@ -37,7 +37,7 @@ export default function NavBar() {
           </div>
           <div className="block sm:hidden">
             <Link href="/" passHref>
-              <span className="btn btn-ghost normal-case text-sm tracking-tight font-mono text-bold">
+              <span className="btn btn-ghost text-left normal-case text-sm tracking-tight font-mono text-bold">
                 Minecraft WikiNote
               </span>
             </Link>
