42c602518c4a921f868950ce60ae966ca60abc4a
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-03 21:07:25 +0900

    Update Convert.js

--- components/Convert.js
+++ components/Convert.js
@@ -60,7 +60,7 @@ export function ContentCode({ children }) {
       <div className="pt-1" />
       <div className="rounded-md bg-base-content pb-3 pt-3 shadow-md shadow-slate-800/50 overflow-x-auto">
         <div className="pl-4 z-10 py-1">
-        <code className="text-base-100 text-mono">{children}</code>
+        <code className="text-base-100 font-mono">{children}</code>
         </div>
       </div>
       <div className="pb-2" />
