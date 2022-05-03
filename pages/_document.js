7ab30d9859cc0840e21305531a5dd59804a257f0
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-03 15:00:28 +0900

    * Add Google Fonts

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
