1a6eda4e333cfc896db6f68d4d43aa73fedce469
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-04 14:02:07 +0900

    * Change responsibility for smartphone

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,58 +2,11 @@ import Link from "next/link";
 export default function NavBar() {
   const lico = "🌞";
   const dico = "🌙";
-  return (
-    <div className="sticky top-0 z-50 w-full">
-      <div className="navbar bg-base-200/95">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex="0" className="btn btn-ghost btn-circle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h7"
-                />
-              </svg>
-            </label>
-            <ul
-              tabIndex="0"
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a>Homepage</a>
-              </li>
-              <li>
-                <a>About</a>
-              </li>
-            </ul>
-          </div>
-          <div className="block sm:hidden">
-            <Link href="/" passHref>
-              <span className="btn btn-ghost text-left normal-case text-sm tracking-tight text-bold">
-                Minecraft WikiNote
-              </span>
-            </Link>
-          </div>
-        </div>
-        <div className="navbar-center hidden sm:block">
-          <div className="hidden sm:block">
-            <Link href="/" passHref>
-              <span className="btn btn-ghost normal-case text-lg text-bold">
-                Minecraft WikiNote
-              </span>
-            </Link>
-          </div>
-        </div>
-        <div className="navbar-end">
-          <button className="btn btn-ghost btn-circle">
+  
+  function navbarend() {
+    return (
+      <>
+      <button className="btn btn-ghost btn-circle">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -110,6 +63,65 @@ export default function NavBar() {
               </li>
             </ul>
           </div>
+        </>
+    )
+  }
+  
+  return (
+    <div className="sticky top-0 z-50 w-full">
+      <div className="navbar bg-base-200/95">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex="0" className="btn btn-ghost btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex="0"
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>Homepage</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+            </ul>
+          </div>
+          <div className="block sm:hidden">
+            <Link href="/" passHref>
+              <span className="btn btn-ghost text-left normal-case text-base tracking-tight text-bold">
+                Minecraft RipeNote
+              </span>
+            </Link>
+            <div className="flex justify-end">
+              {navbarend()}
+            </div>
+          </div>
+        </div>
+        <div className="navbar-center hidden sm:block">
+          <div className="hidden sm:block">
+            <Link href="/" passHref>
+              <span className="btn btn-ghost normal-case text-lg text-bold">
+                Minecraft WikiNote
+              </span>
+            </Link>
+          </div>
+        </div>
+        <div className="navbar-end hidden sm:block">
+          {navbarend()}
         </div>
       </div>
     </div>
