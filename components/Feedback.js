a1fcfa3ed7ffa2b59928507b635240f349adda84
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-05 02:59:22 +0900

    * Reposive Feedback

--- components/Feedback.js
+++ components/Feedback.js
@@ -5,7 +5,7 @@ export default function Feedback() {
         <>
             <input type="checkbox" id="feedback-modal" className="modal-toggle" />
             <label htmlFor="feedback-modal" className="modal cursor-pointer">
-                <label className="modal-box relative h-3/4" htmlFor="">
+                <label className="modal-box relative h-3/4 w-5/6 sm:w-4/6" htmlFor="">
                     <div className="w-full h-full">
                         <iframe width="100%" height="100%" src="https://m.kuku.lu/f.php?796acf2864" />
                     </div>
