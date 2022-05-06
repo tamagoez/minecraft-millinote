8c666af9d9aa0f1c099e4b398abf1e174e330e99
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-06 22:48:34 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap"
           rel="stylesheet"
         />
         <link
