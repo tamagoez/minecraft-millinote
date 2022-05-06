ee5f5cfe11ce44a1165bfa5fa437789408365161
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-06 22:36:17 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Radio+Canada:wght@400&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto+Flex:wght@350&display=swap"
           rel="stylesheet"
         />
         <link
