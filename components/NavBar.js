3a3f7ec708d18ddc4118748a6b1825d5af05bfc9
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-07 14:21:18 +0900

    * Fix mistake

--- components/NavBar.js
+++ components/NavBar.js
@@ -69,7 +69,7 @@ export default function NavBar() {
   
   return (
     <div className="sticky top-0 z-50 w-full">
-      <div className="navbar bg-base-200/85 rounded-b-[32px] backdrop-blur-sm">
+      <div className="navbar bg-base-100/[.85] rounded-b-[32px] backdrop-blur-sm">
         <div className="flex-none">
           <div className="dropdown">
             <label tabIndex="0" className="btn btn-ghost btn-circle">
