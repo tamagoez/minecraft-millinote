27c5f67ad2a56a5f47d594b8f9d69f02477bfa30
tamagoez <87574268+tamagoez@users.noreply.github.com>
2022-05-08 10:39:19 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -39,7 +39,6 @@ export function HeadContents({ address }) {
   return (
     <>
       <Head>
-        <meta name="description" content="Minecraft wiki which ripe in knowledge and experience." />
         <meta property="og:type" content="website" />
         <meta property="og:image" content={imgUrl} />
         <link rel="icon" href="/favicon.ico" />
